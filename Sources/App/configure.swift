da8e7f76df309385dfad66ccc00c06df752494f3
Dmitriy Holovnia <52383559+Dmitriy-Holovnia@users.noreply.github.com>
2021-07-27 19:41:30 +0300

    initial commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import Fluent
 import FluentPostgresDriver
+import FluentSQLiteDriver
 import Vapor
 
 // configures your application
@@ -7,16 +8,18 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+//    app.databases.use(.postgres(
+//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+//    ), as: .psql)
+    app.databases.use(.sqlite(.memory), as: .sqlite)
 
-    app.migrations.add(CreateTodo())
-
-    // register routes
+    app.migrations.add(CreateAcronym())
+    app.logger.logLevel = .debug
+    try app.autoMigrate().wait()
+    
     try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,68 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
+    app.get { req -> String in
         return "It works!"
     }
-
+    
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
-
-    try app.register(collection: TodoController())
+    
+    // Create
+    app.post("api", "acronyms") { request -> EventLoopFuture<Acronym> in
+        let acronym = try request.content.decode(Acronym.self)
+        return acronym.save(on: request.db).map({ acronym })
+    }
+    
+    // Get all
+    app.get("api", "acronyms") { request -> EventLoopFuture<[Acronym]> in
+        Acronym.query(on: request.db).all()
+    }
+    
+    // Get by id
+    app.get("api", "acronyms", ":acronymID") { request -> EventLoopFuture<Acronym> in
+        Acronym.find(request.parameters.get("acronymID"), on: request.db)
+            .unwrap(or: Abort(.notFound))
+    }
+    
+    // Get first
+    app.get("api", "acronyms", "first") { request -> EventLoopFuture<Acronym> in
+        Acronym.query(on: request.db)
+            .first()
+            .unwrap(or: Abort(.notFound))
+    }
+    
+    // Update
+    app.put("api", "acronyms", ":acronymID") { request -> EventLoopFuture<Acronym> in
+        let updatedAcronym = try request.content.decode(Acronym.self)
+        return Acronym.find(request.parameters.get("acronymID"), on: request.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { acronym in
+                acronym.long = updatedAcronym.long
+                acronym.short = updatedAcronym.short
+                return acronym.save(on: request.db)
+                    .map({ acronym })
+            }
+    }
+    
+    // Delete
+    app.delete("api", "acronyms", ":acronymID") { request -> EventLoopFuture<HTTPStatus> in
+        Acronym.find(request.parameters.get("acronymID"), on: request.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap({ $0.delete(on: request.db).transform(to: .noContent) })
+    }
+    
+    // Filter
+    app.get("api", "acronyms", "search") { request -> EventLoopFuture<[Acronym]> in
+        guard let searchTerm = request.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        
+        return Acronym.query(on: request.db).group(.or) { or in
+            or.filter(\.$short == searchTerm)
+            or.filter(\.$long == searchTerm)
+        }
+        .all()
+    }
 }
